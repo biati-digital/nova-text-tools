642a1e75a3742a0f695bb27a524a843e95828c83
biati-digital <manuel@biati.digital>
2021-10-25 18:36:26 +0000

    Added esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 const commonjs = require('@rollup/plugin-commonjs');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const json = require('@rollup/plugin-json');
+const esbuild = require('rollup-plugin-esbuild');
 
 module.exports = {
     input: 'Scripts/main.js',
     plugins: [
+        esbuild({
+            include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+            minify: false,
+            target: 'es2015'
+        }),
         commonjs(),
         nodeResolve({
             browser: true
